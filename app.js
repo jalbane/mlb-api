e55c4aa7cbf171e6c523c47c9194f0ddfa19e61c
Jeff Albanese <jalbane9@gmail.com>
2021-08-06 12:26:40 -0400

    Comment out dotenv to deploy to heroku

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 80;
 const MongoClient = require('mongodb').MongoClient;
 const cors = require('cors');
-require('dotenv').config();
+//require('dotenv').config();
 var dbConnect;
 
 app.use(cors())
